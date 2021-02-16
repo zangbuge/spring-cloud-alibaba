b373330a9f894883b6a16b13d894ca72d9c0af49
lihuiming <1469987440@qq.com>
2021-02-16 17:07:30 +0800

    熔断

--- feign/src/main/java/com/zangbuge/feign/test/ProviderClient.java
+++ feign/src/main/java/com/zangbuge/feign/test/ProviderClient.java
@@ -3,7 +3,7 @@ package com.zangbuge.feign.test;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-/**
+/** 熔断 @FeignClient 指定 fallback = FallbackService.class  FallbackService 实现ProviderClient接口
  * @Author: Li Huiming
  * @Date: 2021/2/6
  */
